48311b2d5075771900528fb7b608ae0388575f8d
Andrea Benedetti <anbened@live.com>
2019-11-15 13:10:36 +0100

    Update DatabaseServiceObjectives.sql

--- DatabaseServiceObjectives.sql
+++ DatabaseServiceObjectives.sql
@@ -1,22 +1,31 @@
 
 /*** Connect to MASTER ***/
-
 /*
 Returns info like edition (service tier), pricing tier and elastic pool name, if any, ... 
 for an Azure SQL database or an Azure SQL Data Warehouse
 */
 
-select 
-	d.name as [DB Name],
-	so.edition as [Service Tier],
-	so.service_objective as [Pricing Tier],
-	d.create_date as [DB Create Date],
-	d.compatibility_level as [DB Compatibility Level],
-	case (d.is_encrypted) when 1 then 'Yes' else 'No' end as [Encrypted],
-	case (DatabasePropertyEx(DB_Name(), 'IsXTPSupported')) when 1 then 'Yes' else 'No' end as [In-Memory Support]
-FROM sys.databases d   
-JOIN sys.database_service_objectives so 
-ON d.database_id = so.database_id
-where edition != 'System'
-order by d.name
-
+SELECT d.name AS [DB Name], 
+       so.edition AS [Service Tier], 
+       so.service_objective AS [Pricing Tier], 
+       d.create_date AS [DB Create Date], 
+       d.compatibility_level AS [DB Compatibility Level],
+       CASE(d.is_encrypted)
+           WHEN 1
+           THEN 'Yes'
+           ELSE 'No'
+       END AS [Encrypted],
+       CASE(DATABASEPROPERTYEX(DB_NAME(), 'IsXTPSupported'))
+           WHEN 1
+           THEN 'Yes'
+           ELSE 'No'
+       END AS [In-Memory Support], 
+       (CASE
+            WHEN so.elastic_pool_name IS NULL
+            THEN 'No Elastic Pool used'
+            ELSE so.elastic_pool_name
+        END) AS [Elastic Pool Info]
+FROM sys.databases d
+     JOIN sys.database_service_objectives so ON d.database_id = so.database_id
+WHERE edition != 'System'
+ORDER BY d.name;
